aee30e902e596e71e35474c0c6944d0035679c6f
mykhailenko.s@ukr.net <mykhailenko.s@ukr.net>
2016-04-18 19:25:03 +0300

    One test and abstract class are added.

---
+++ RectangleMethod/src/test/CenterRectangleMethodTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import com.goit.rectanglemethod.CenterRectangleMethod;
+import com.goit.rectanglemethod.ConvertFunctionToRPN;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Сергей on 18.04.2016.
+ */
+public class CenterRectangleMethodTest {
+
+    private static   String expression="x+2";
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+    @Test
+    public void testCenterCornerRM() throws Exception {
+        double lowerBorder=5;
+        double upperBorder=10;
+        int countOfSteps=2;
+        CenterRectangleMethod centerRectangleMethod = new CenterRectangleMethod(expression);
+        ConvertFunctionToRPN convertFunctionToRPN = new ConvertFunctionToRPN();
+        Double result;
+        Double deltaX = (upperBorder - lowerBorder) / countOfSteps;
+        Double[] array = new Double[countOfSteps];
+        array[0] = lowerBorder+deltaX;
+        result = deltaX * convertFunctionToRPN.calculateIntegral(expression, (array[0] - deltaX / 2));
+        for (int i = 1; i<=countOfSteps-1; i++){
+            array[i] = array[i-1]+ deltaX;
+            result = result + deltaX * convertFunctionToRPN.calculateIntegral(expression, (array[i] - deltaX / 2));
+        }
+
+        Assert.assertEquals(47.5,result, 0);
+    }
+}
