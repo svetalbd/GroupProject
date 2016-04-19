710beff2805d0c83144de36495ac28793bbab246
mykhailenko.s@ukr.net <mykhailenko.s@ukr.net>
2016-04-19 22:53:20 +0300

    Added test for ConvertToRPN

---
+++ RectangleMethod/src/test/RightRectangleMethodTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import com.goit.rectanglemethod.AbstractRectangleMethod;
+
+/**
+ * Created by ZahornyiAI on 19.04.2016.
+ */
+class RightRectangleMethod extends AbstractRectangleMethod {
+    public RightRectangleMethod(String expression) {
+        super(expression);
+    }
+
+    public double rightCornerRM (Double lowerBorder, Double upperBorder, Integer countOfSteps) {
+
+        Double result;
+        Double deltaX = (upperBorder - lowerBorder) / countOfSteps;
+        Double[] array = new Double[countOfSteps];
+        array[0] = lowerBorder+deltaX;
+        result = deltaX * convertFunctionToRPN.calculateIntegral(expression, array[0]);
+        for (int i = 1; i<=countOfSteps-1; i++){
+            array[i] = array[i-1]+ deltaX;
+            result = result + deltaX * convertFunctionToRPN.calculateIntegral(expression, array[i]);
+
+        }
+        return result;
+    }
+}
