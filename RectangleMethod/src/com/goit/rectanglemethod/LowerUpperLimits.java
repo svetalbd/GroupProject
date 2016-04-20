07cbeeb1cb25030b4e9a5e167b51718cc3331faf
Svitlana Mykhailenko <mykhailenko.s@ukr.net>
2016-04-20 17:17:31 +0300

    Update LowerUpperLimits.java

--- RectangleMethod/src/com/goit/rectanglemethod/LowerUpperLimits.java
+++ RectangleMethod/src/com/goit/rectanglemethod/LowerUpperLimits.java
@@ -1,7 +1,7 @@
 package com.goit.rectanglemethod;
 
 /**
- * Created by Сергей on 14.04.2016.
+ * Created by Mykhailenko Svitlana on 14.04.2016.
  */
 public class LowerUpperLimits {
     private double lowerBorder;
